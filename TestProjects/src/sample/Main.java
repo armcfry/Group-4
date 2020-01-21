7acf51d48923785bf57a3b7cef5ca61f7bf77287
T.J. Mosser <tjmosser@gmail.com>
2020-01-20 20:49:25 -0600

    IN PROGRESS: Implementing Resizing of Rooms
    Added semi-transparent circles to edges and center of room. Experimenting with resizing functionality of rooms. This is still in progress.

--- TestProjects/src/sample/Main.java
+++ TestProjects/src/sample/Main.java
@@ -26,77 +26,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*
-        // Test for a line
-        javafx.scene.shape.Line line = new javafx.scene.shape.Line();
-
-        line.setStartX(100.0);
-        line.setStartY(150.0);
-        line.setEndX(500.0);
-        line.setEndY(150.0);
-
-        //Group root = new Group(Collections.singleton(line));
-
-        Room room = new Room(100, 50);
-
-        //Scene scene = new Scene(room, 750, 750);
-
-//        scene.setOnMouseReleased(event -> {
-//            room.Remove();
-//        });
-
-        TabPane tabPane = new TabPane();
-        Tab tab = new Tab();
-        tab.setText("new tab");
-        tab.setContent(new Room(100, 100));
-        tabPane.getTabs().add(tab);
-
-        //Scene scene = new Scene(tabPane, 750, 750);
-        Scene scene = new Scene(room, 750, 750);
-
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-
-        /*Pane mainPane = new Pane();
-        mainPane.setPrefWidth(400);
-        mainPane.setPrefHeight(600);
-
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        menuBar.getMenus().add(fileMenu);
-        mainPane.getChildren().add(menuBar);
-        menuBar.setPrefWidth(32.0);
-        menuBar.setPrefHeight(600.0);
-
-        ScrollPane designerPane = new ScrollPane();
-        designerPane.setPrefViewportWidth(330);
-        designerPane.setPrefViewportHeight(450);
-        SubScene canvas = new SubScene(designerPane, 500, 500);
-
-        Scene scene = new Scene(mainPane, 400, 600);*/
 
         Parent root = FXMLLoader.load(getClass().getResource("finalversion.fxml"));
-
-        /*ScrollPane sP = (ScrollPane)root.getChildrenUnmodifiable().get(2);
-        sP.setContent(new SubScene(new Room(100, 50), 500, 500));*/
-
-//        sP.getContent().relocate(sP.getLayoutX(), sP.getLayoutY());
-        /*MenuBar menuBar = (MenuBar)root.getChildrenUnmodifiable().get(1);
-
-        sP.getContent().relocate(sP.getLayoutX(), sP.getLayoutY());
-
-        sP.getContent().setPickOnBounds(true);*/
 
         ScrollPane scrollPane = (ScrollPane)root.getChildrenUnmodifiable().get(2);
         String s = scrollPane.getContent().getId();
         SubScene subScene = (SubScene)scrollPane.getContent();
-        subScene.setRoot(new Room(100, 50));
+        subScene.setRoot(new Room(100, 100, 100, 50));
 
-        System.out.println(s);
-
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Roomba Simulator 3000");
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
--- TestProjects/src/sample/Room.java
+++ TestProjects/src/sample/Room.java
@@ -4,26 +4,57 @@ import java.awt.*;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
 import java.math.*;
 
+/*
+ * TODO: Add blue circles for each edge and the center for movement and resizing.
+ *  TODO: Clicking and dragging on edge circles should increase width/height by one grid
+ *   unit.
+ *   TODO: Clicking and dragging on the center circle should move the shape along around in the scene
+ * */
+
 public class Room extends Group {
 
-    public Room(int width, int height) {
+    public Room(int x, int y, int width, int height) {
+        this.centerX = x;
+        this.centerY = y;
         this.width = width;
         this.height = height;
 
+        // Create the room's outline
         room = new Rectangle();
         room.setWidth(width);
         room.setHeight(height);
-        room.setX(0);
-        room.setY(0);
-        room.setFill(javafx.scene.paint.Color.RED);
+        room.setX(centerX - (width / 2.0));
+        room.setY(centerY - (height / 2.0));
+        room.setFill(Color.TRANSPARENT);
+        room.setStroke(Color.BLACK);
+        room.setStrokeWidth(1);
+
+        // Instantiate movement circles, but make them invisible
+        resizingMarkers = new Circle[5];
+        resizingMarkers[0] = new Circle(centerX, centerY, 5);
+        resizingMarkers[0].setFill(new Color(0.0, 0.0, 1.0, 0.5));
+
+        resizingMarkers[1] = new Circle(centerX - (width / 2.0), centerY, 5);
+        resizingMarkers[1].setFill(new Color(0.0, 0.0, 1.0, 0.5));
+
+        resizingMarkers[2] = new Circle(centerX, centerY + (height / 2.0), 5);
+        resizingMarkers[2].setFill(new Color(0.0, 0.0, 1.0, 0.5));
+
+        resizingMarkers[3] = new Circle(centerX + (width / 2.0), centerY, 5);
+        resizingMarkers[3].setFill(new Color(0.0, 0.0, 1.0, 0.5));
+
+        resizingMarkers[4] = new Circle(centerX, centerY - (height / 2.0), 5);
+        resizingMarkers[4].setFill(new Color(0.0, 0.0, 1.0, 0.5));
+
+        getChildren().addAll(resizingMarkers);
 
         getChildren().add(room);
-        //getChildren().add(path);
         makeDraggable();
     }
 
@@ -40,8 +71,17 @@ public class Room extends Group {
         this.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                room.setX(Math.round(event.getSceneX()/*/50.0) * 50*/));
-                room.setY(Math.round(event.getSceneY()/*/50.0) * 50*/));
+                //room.setX(Math.round(event.getSceneX()));
+                //room.setY(Math.round(event.getSceneY()));
+
+                // TODO: Check if the user clicked the room. If so, display all of the circles and detect mouse clicks.
+                // TODO: If the user clicks on a circle, determine which one should be affecting the drag function.
+                for (Circle cir : resizingMarkers) {
+                    if (cir.contains(event.getX(), event.getY())) {
+                        /* TODO: Find some way to set a state for the mouse drag handler to determine how to resize
+                        *   the shape. */
+                    }
+                }
 
                 event.consume();
             }
@@ -50,8 +90,27 @@ public class Room extends Group {
         this.setOnMouseDragged(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                room.setX((Math.round(event.getX()/25.0) * 25) - (width / 2));
-                room.setY((Math.round(event.getY()/25.0) * 25) - (height / 2));
+                double newX = (Math.round(event.getX()/25.0) * 25);
+                double newY = (Math.round(event.getY()/25.0) * 25);
+
+                room.setX(newX - (width / 2.0));
+                room.setY(newY - (height / 2.0));
+
+                // Change the positions of the circles
+                resizingMarkers[0].setCenterX(newX);
+                resizingMarkers[0].setCenterY(newY);
+
+                resizingMarkers[1].setCenterX(newX - (width / 2.0));
+                resizingMarkers[1].setCenterY(newY);
+
+                resizingMarkers[2].setCenterX(newX);
+                resizingMarkers[2].setCenterY(newY + (height / 2.0));
+
+                resizingMarkers[3].setCenterX(newX + (width / 2.0));
+                resizingMarkers[3].setCenterY(newY);
+
+                resizingMarkers[4].setCenterX(newX);
+                resizingMarkers[4].setCenterY(newY - (height / 2.0));
 
                 event.consume();
             }
@@ -59,6 +118,8 @@ public class Room extends Group {
     }
 
     int width, height;
+    int centerX, centerY;
     Rectangle room;
+    Circle[] resizingMarkers;
 
 }
